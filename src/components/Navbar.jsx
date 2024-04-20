d3ac8294ecd35dd6dec62ae3f90dfcfc2bca5194
kbodur <kbodur@pursuit.org>
2024-04-19 22:47:05 -0400

    added live URL

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='flag' src="../src/assets/image.png" width={'30px'} />
+      <img className='flag' src="./src/assets/image.png" width={'30px'} />
     </div>
   )
 }
